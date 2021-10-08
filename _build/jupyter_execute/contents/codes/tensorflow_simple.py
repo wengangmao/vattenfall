c784c55ffe2ea373ba6918bd4ad8552bbd4cd0a5
wengang mao <wengang.mao@chalmers.se>
2021-10-08 15:10:25 +0200

    Model good results

--- _build/jupyter_execute/contents/codes/tensorflow_simple.py
+++ _build/jupyter_execute/contents/codes/tensorflow_simple.py
@@ -79,95 +79,43 @@ sns.heatmap(df_train.corr(), annot=True, fmt=".2f")
 plt.show()
 
 
-# In[51]:
+# In[5]:
 
 
-plt.hist2d(target, df_train['guide_open'], bins=(50, 50))
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import colors, ticker, cm
+
+
+fig = plt.figure(figsize=(30, 10))
+fig.add_subplot(131)
+plt.hist2d(df['head_gross'], df['guide_open'], bins=(25, 25))
+plt.xlabel('head_gross', size=20)
+plt.ylabel('guide_open', size=20)
 plt.colorbar()
-plt.xlabel('Head_{net}')
-plt.ylabel('Guide_{open}')
+
+fig.add_subplot(132)
+plt.hist2d(df['head_gross'], df['pump101_speed'], bins=(25, 25))
+plt.xlabel('head_gross', size=20)
+plt.ylabel('pump101_speed', size=20)
+plt.colorbar()
+
+fig.add_subplot(133)
+plt.hist2d(df['head_gross'], df['discharge_rate'], bins=(50, 50))
+plt.xlabel('head_gross', size=20)
+plt.ylabel('discharge_rate', size=20)
+plt.colorbar()
 
 
-# ### 2.3, First attemp to get the model Head_net_t = f(guide_open_{t-1, t-2, ...}, pump01_speed_{t-t, t-2, ...}, discharge_rate_{t-1, t-2, ...},...)
-
-# In[9]:
+# In[6]:
 
 
-df_train = df_train.reset_index(drop=True)
-target = df_train.pop('head_net')
-
-
-# In[42]:
-
-
-# Pre-normalize the features for ML analysis
-norm = tf.keras.layers.Normalization(axis=-1)
-#normalizer.adapt(df_train)
-BATCH_SIZE = 200
-
-# normalize the target (output)
-norm_target = tf.keras.layers.Normalization(axis=None)
-norm_target.adapt(target)
-tf_target = norm_target(target)
-
-def get_basic_model():
-  model = tf.keras.Sequential([
-    norm,
-    tf.keras.layers.Dense(10, activation='relu'),
-    tf.keras.layers.Dense(10, activation='relu'),
-    tf.keras.layers.Dense(1)
-  ])
-
-  model.compile(optimizer='adam',
-                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                metrics=['accuracy'])
-  return model
-
-model = get_basic_model()
-model.fit(df_train, tf_target, epochs=10, batch_size=BATCH_SIZE)
-
-
-# In[44]:
-
-
-plt.plot(model.predict(tf_train),'b')
-plt.plot(tf_target,'k')
-plt.show()
-
-
-# In[45]:
-
-
-norm1 = tf.keras.layers.Normalization(axis =-1)
-norm1.adapt(df_train)
-tf_train = norm1(df_train)
-
-norm2 = tf.keras.layers.Normalization(axis = None)
-norm2.adapt(target)
-tf_target= norm2(target)
-
-dataset_df = tf.data.Dataset.from_tensor_slices((tf_train, tf_target))
-dataset_batches = dataset_df.shuffle(1000).batch(BATCH_SIZE)
-
-model = get_basic_model()
-model.fit(dataset_batches, epochs=5)
-
-
-# In[48]:
-
-
-tf.keras.utils.plot_model(model, rankdir="LR", show_shapes=True)
-
-
-# In[102]:
-
-
-
+features = df.keys()
+features
 
 
 # ### 2.2, autocorrelation function (ACF) and (PACF) to check time dependence
 
-# In[101]:
+# In[7]:
 
 
 def autocorr(x):
@@ -201,7 +149,7 @@ plt.show()
 
 # ### 2.3 crossing autocorrelationship for various parameters
 
-# In[151]:
+# In[9]:
 
 
 # NB: we have resampled the data for the autocorreltion analysis
@@ -239,12 +187,12 @@ for p in acf2:
     i= i + 1
 ax3.grid(True)
 ax3.set_title("ACF ofHead_net", fontsize=20, color='blue')
-ax4.set_xlabel(r'Time Lag: sample frequency $\times$ 5000', fontsize=20, color='blue')
+ax3.set_xlabel(r'Time Lag: sample frequency $\times$ 5000', fontsize=20, color='blue')
 
 plt.show()
 
 
-# In[152]:
+# In[10]:
 
 
 # Autocorrelation between guide_opan and pump101_speed
@@ -289,9 +237,9 @@ ax3.set_xlabel(r'Time Lag: sample frequency $\times$ 5000', fontsize=20, color='
 plt.show()
 
 
-# ### 2.3, normalize the data
+# ### 2.4, normalize the data
 
-# In[15]:
+# In[11]:
 
 
 # First, we assume all data are used for the training (the time series is not that stationary for the prediction)
@@ -308,7 +256,7 @@ ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
 fig3 = ax.set_xticklabels(train_df.keys(), rotation=90)
 
 
-# In[42]:
+# In[12]:
 
 
 # tf.convert_to_tensor(new_df)
@@ -322,73 +270,52 @@ new_df = tf.convert_to_tensor(new_df)
 #normalizer.adapt(numeric_features)
 
 
-# In[50]:
+# In[13]:
 
 
-tf.data.Dataset.from_tensor_slices(new_df)
+feature_keys
 
 
-# ## Test the functions of the tf.data.Dataset for slice data to formulate rolling windowed dataset
+# ## 3, RNN deep learning to build the model by three different approaches. 
+#    **1. normal deep learning network**<br />
+#    **2. RNN**<br />
+#    **3. ARIMA model**<br />
 
-# In[199]:
+# ### 3.1, First attemp to the model $Head\_net_t = f(guide\_open_{(t-1, t-2, ...)}, pump\_speed_{(t-1, t-2, ...)}, discharge\_rate_{(t-1, t-2, ...)},...)$
 
-
-test = tf.data.Dataset.from_tensor_slices(train_df)
-train_df.info()
-
-
-# ## <span style ="color:blue; font-size:25px"> **3, NB: get the rolling/windowed dataset**</span>
-
-# In[96]:
+# In[14]:
 
 
-def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
-  """Function for creating a windowed dataset for sequence training"""
-  dataset = tf.data.Dataset.from_tensor_slices(series)
-  dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
-  dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
-  dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1]))
-  dataset = dataset.batch(batch_size).prefetch(1)
-  return dataset
+df_train = df[feature_keys][int(len(df)/4):int(len(df)/2):100]
+
+df_train = df_train.reset_index(drop=True)
+#df_train.pop('time')
+df_train.pop('head_net')
+df_target = df_train.pop('head_gross')
+df_train.head()
 
 
-# In[132]:
+# In[15]:
 
 
-series = np.random.normal(0, 1, 19)
-series
+# Pre-normalize the features for ML analysis
+norm = tf.keras.layers.Normalization(axis=-1)
+norm.adapt(df_train)
+tf_train = norm(df_train)
 
+# normalize the target (output)
+norm_target = tf.keras.layers.Normalization(axis=None)
+norm_target.adapt(df_target)
+tf_target = norm_target(df_target)
 
-# In[187]:
-
-
-data = np.array(train_df.guide_open, dtype=np.float32)
-
-tf.keras.backend.clear_session()
-tf.random.set_seed(51)
-np.random.seed(51)
-shuffle_buffer_size = 3
-window_size = 80
-batch_size = 150
-train_set  = windowed_dataset(data, window_size, batch_size, shuffle_buffer_size)
-n = 0
-for window in train_set:
-    n = n +1
-    print(window)
-
-display(Markdown('<span style="color:blue; font-size:20px">The windowed size is: %d, and raw datashape is (%d)</span>'%(n, data.shape[0])))
-
-#(f'The windowed size and raw datashape:{n, data.shape}')
-
-
-# In[ ]:
-
+# configure the model
+BATCH_SIZE = 500
 
 def get_basic_model():
   model = tf.keras.Sequential([
-    tf.data.Dataset.from_tensor_slices(new_df),
-    tf.keras.layers.Dense(10, activation='relu'),
-    tf.keras.layers.Dense(10, activation='relu'),
+    norm,
+    tf.keras.layers.Dense(50, activation='relu'),
+    tf.keras.layers.Dense(50, activation='relu'),
     tf.keras.layers.Dense(1)
   ])
 
@@ -398,8 +325,345 @@ def get_basic_model():
   return model
 
 model = get_basic_model()
-model.add(new_df.layers[0])
-model.fit(tf.data.Dataset.from_tensor_slices(new_df), target, epochs=3, batch_size=BATCH_SIZE)
+model.fit(tf_train, tf_target, epochs=10, batch_size=BATCH_SIZE)
+
+
+# In[16]:
+
+
+plt.plot(model.predict(df_train),'b')
+plt.plot(tf_target,'k')
+plt.show()
+
+
+# In[17]:
+
+
+norm1 = tf.keras.layers.Normalization(axis =-1)
+norm1.adapt(df_train)
+tf_train = norm1(df_train)
+
+norm2 = tf.keras.layers.Normalization(axis = None)
+norm2.adapt(df_target)
+tf_target= norm2(df_target)
+
+dataset_df = tf.data.Dataset.from_tensor_slices((tf_train, tf_target))
+dataset_batches = dataset_df.shuffle(100).batch(BATCH_SIZE)
+
+model = get_basic_model()
+model.fit(dataset_batches, epochs=10)
+
+
+# ### 3.2, Understand datastructure of the tensorflow package --> formulate rolling windowed dataset --> for model in Section 4
+
+# In[18]:
+
+
+# split the time series of data into train (70%), test (20%) and validation (10%)
+#n = len(df_train)
+df_train = df[feature_keys]
+df_train.pop('head_net')
+df_train.pop('discharge_rate')
+n = len(df_train)
+# start to divide them
+train_df = df_train[0:int(n*0.7)]
+test_df = df_train[int(n*0.7):int(n*0.9)]
+val_df = df_train[int(n*0.9):]
+df_train.shape[1]
+
+## Tensorflow dataset donot have buildin functions to estimate the mean and standard deviation
+#normalizer = tf.keras.layers.Normalization(axis=-1)
+#normalizer.adapt(train_df)
+#train_df = normalizer(train_df)
+
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean)/train_std
+test_df = (test_df - train_mean)/train_std
+val_df = (val_df - train_mean)/train_std
+
+df_std = (df_train - train_mean) / train_std
+df_std = df_std.melt(var_name='Column', value_name='Normalized')
+
+fig, ax = plt.subplots(figsize=[20, 10])
+ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+ax.set_xticklabels(ax.get_xticks(), size = 30)
+ax.set_yticklabels(ax.get_yticks(), size = 30)
+
+
+# ### <span style ="color:blue; font-size:25px"> **3.3, get the rolling/windowed dataset**</span>
+
+# In[19]:
+
+
+# 3.3.1, Indexes and offsets
+
+## NB: the data for training, testing and validation should be store here first!!
+class WindowGenerator():
+  def __init__(self, input_width, label_width, shift,
+               train_df=train_df, val_df=val_df, test_df=test_df,
+               label_columns=None):
+    # Store the raw data.
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
+
+    # Work out the label column indices.
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
+
+    # Work out the window parameters.
+    self.input_width = input_width
+    self.label_width = label_width
+    self.shift = shift
+
+    self.total_window_size = input_width + shift
+
+    self.input_slice = slice(0, input_width)
+    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+    self.label_start = self.total_window_size - self.label_width
+    self.labels_slice = slice(self.label_start, None)
+    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+  def __repr__(self):
+    return '\n'.join([
+        f'Total window size: {self.total_window_size}',
+        f'Input indices: {self.input_indices}',
+        f'Label indices: {self.label_indices}',
+        f'Label column name(s): {self.label_columns}'])
+
+# 3.3.2, Split the window
+def split_window(self, features):
+  inputs = features[:, self.input_slice, :]
+  labels = features[:, self.labels_slice, :]
+  if self.label_columns is not None:
+    labels = tf.stack(
+        [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+        axis=-1)
+
+  # Slicing doesn't preserve static shape information, so set the shapes
+  # manually. This way the `tf.data.Datasets` are easier to inspect.
+  inputs.set_shape([None, self.input_width, None])
+  labels.set_shape([None, self.label_width, None])
+
+  return inputs, labels
+
+WindowGenerator.split_window = split_window
+
+
+# 3.3.3, plot the time series
+def plot(self, model=None, plot_col='head_gross', max_subplots=3):
+  inputs, labels = self.example
+  plt.figure(figsize=(12, 8))
+  plot_col_index = self.column_indices[plot_col]
+  max_n = min(max_subplots, len(inputs))
+  for n in range(max_n):
+    plt.subplot(max_n, 1, n+1)
+    plt.ylabel(f'{plot_col} [normed]')
+    plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+             label='Inputs', marker='.', zorder=-10)
+
+    if self.label_columns:
+      label_col_index = self.label_columns_indices.get(plot_col, None)
+    else:
+      label_col_index = plot_col_index
+
+    if label_col_index is None:
+      continue
+
+    plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                edgecolors='k', label='Labels', c='#2ca02c', s=64)
+    if model is not None:
+      predictions = model(inputs)
+      plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                  marker='X', edgecolors='k', label='Predictions',
+                  c='#ff7f0e', s=64)
+
+    if n == 0:
+      plt.legend()
+
+  plt.xlabel('Time [minisecond]')
+
+WindowGenerator.plot = plot
+
+
+# In[21]:
+
+
+# Test of windowed dataset 
+
+#w0 = WindowGenerator(input_width=24, label_width=1, shift=24, label_columns=['head_gross'])
+w1 = WindowGenerator(input_width=24, label_width=1, shift=24, label_columns=None)
+w=w1
+
+# Stack three slices, the length of the total window.
+example_window = tf.stack([np.array(train_df[:w.total_window_size]),
+                           np.array(train_df[100:100+w.total_window_size]),
+                           np.array(train_df[200:200+w.total_window_size])])
+
+example_inputs, example_labels = w.split_window(example_window)
+
+print('All shapes are: (batch, time, features)')
+print(f'Window shape: {example_window.shape}')
+print(f'Inputs shape: {example_inputs.shape}')
+print(f'Labels shape: {example_labels.shape}')
+
+
+# In[23]:
+
+
+w.example = example_inputs, example_labels
+w.plot(plot_col='guide_open')
+
+
+# ### <span style ="color:blue; font-size:25px"> **3.4, Create tf.data.Datasets**</span>
+
+# In[28]:
+
+
+def make_dataset(self, data):
+  data = np.array(data, dtype=np.float32)
+  ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+      data=data,
+      targets=None,
+      sequence_length=self.total_window_size,
+      sequence_stride=1,
+      shuffle=True,
+      batch_size=32,)
+
+  ds = ds.map(self.split_window)
+
+  return ds
+
+WindowGenerator.make_dataset = make_dataset
+
+# add properties to the dataset
+@property
+def train(self):
+  return self.make_dataset(self.train_df)
+
+@property
+def val(self):
+  return self.make_dataset(self.val_df)
+
+@property
+def test(self):
+  return self.make_dataset(self.test_df)
+
+@property
+def example(self):
+  """Get and cache an example batch of `inputs, labels` for plotting."""
+  result = getattr(self, '_example', None)
+  if result is None:
+    # No example batch was found, so get one from the `.train` dataset
+    result = next(iter(self.train))
+    # And cache it for next time
+    self._example = result
+  return result
+
+WindowGenerator.train = train
+WindowGenerator.val = val
+WindowGenerator.test = test
+WindowGenerator.example = example
+
+# Run the example to get the index
+for example_inputs, example_labels in w.train.take(1):
+  print(f'Inputs shape (batch, time, features): {example_inputs.shape}')
+  print(f'Labels shape (batch, time, features): {example_labels.shape}')
+
+
+# In[38]:
+
+
+for example_inputs, example_labels in w.train.take(1):
+  print(f'Inputs shape (batch, time, features): {example_inputs.shape}')
+  print(f'Labels shape (batch, time, features): {example_labels.shape}')
+
+
+# ## 4, **Preliminary model construction**
+
+# ### 4.1, single step model (similar as the conventional independent ML methods)
+
+# #### Test 1: single output window
+
+# In[57]:
+
+
+# Get the first dataset (all features as output)
+single_step_window = WindowGenerator(
+    input_width=1, label_width=1, shift=1,
+    label_columns=['head_gross']) # we can set "label_columns=None"
+single_step_window
+
+for example_inputs, example_labels in single_step_window.train.take(1):
+  print(f'Inputs shape (batch, time, features): {example_inputs.shape}')
+  print(f'Labels shape (batch, time, features): {example_labels.shape}')
+    
+single_step_window.label_columns, single_step_window.column_indices
+
+
+# In[80]:
+
+
+# Baseline model
+class Baseline(tf.keras.Model):
+  def __init__(self, label_index=None):
+    super().__init__()
+    self.label_index = label_index
+
+  def call(self, inputs):
+    if self.label_index is None:
+      return inputs
+    result = inputs[:, :, self.label_index]
+    return result[:, :, tf.newaxis]
+
+# Build the model
+#baseline = Baseline(label_index=single_step_window.column_indices['head_gross'])
+
+baseline = Baseline(label_index=None)
+
+baseline.compile(loss=tf.losses.MeanSquaredError(),
+                 metrics=[tf.metrics.MeanAbsoluteError()])
+
+val_performance = {}
+performance = {}
+val_performance['Baseline'] = baseline.evaluate(single_step_window.val)
+performance['Baseline'] = baseline.evaluate(single_step_window.test, verbose=0)
+
+
+# In[ ]:
+
+
+
+
+
+# #### Test 2: wide output window
+
+# In[97]:
+
+
+wide_window = WindowGenerator(
+    input_width=300, label_width=300, shift=2,
+    label_columns=None) #['head_gross'])    ################# NB: it is very important to choose the parameters here ##############
+###################################################################################################################################
+
+
+wide_window
+print('Input shape:', wide_window.example[0].shape)
+print('Output shape:', baseline(wide_window.example[0]).shape)
+wide_window.plot(baseline)
+
+
+# In[ ]:
+
+
+
 
 
 # ## **4, Model construction**
